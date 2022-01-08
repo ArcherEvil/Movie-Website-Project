f1835a666e6b69659ddea7cc1bba93e40a6de3ab
Archer <archerevil2005@gmail.com>
2022-01-08 23:48:33 +0000

    Created Movie Class

---
+++ Movie WebApp/Models/movies.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie_WebApp.Models
+{
+    public class movies
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+    }
+}
