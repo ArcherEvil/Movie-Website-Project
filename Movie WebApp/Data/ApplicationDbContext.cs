ec1ed7d6a1d47a83266feed8e5bc28f0fb4a59bd
Archer <archerevil2005@gmail.com>
2022-01-09 01:39:37 +0000

    Database Initialized

---
+++ Movie WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_WebApp.Models;
+
+namespace Movie_WebApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
--- Movie WebApp/Models/movies.cs
+++ Movie WebApp/Models/movies.cs
@@ -1,11 +1,17 @@
-﻿namespace Movie_WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_WebApp.Models
 {
-    public class movies
+    public class Movies
     {
+        [Key]
         public int id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Author { get; set; }
         public int Year { get; set; }
+        [Required]
+        public bool IsAvailable { get; set; }
 
     }
 }
